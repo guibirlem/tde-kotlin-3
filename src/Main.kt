3dedb711dd8e361b902cce986d35e43127fa8e49
Guilherme Birlem Machado <132947485+guibirlem@users.noreply.github.com>
2026-03-10 20:11:05 -0300

    tde

--- src/Main.kt
+++ src/Main.kt
@@ -21,6 +21,51 @@ data class Pessoa (val nome: String, val idade: Int , var profissao: String)
     fun falar() = println("falando")
 }
 //
+fun main2 () {
+    val resultTest :Result = Result.Sucess("Sucesso")
+    val teste = when(resultTest) {
+        is Result.Sucess -> println("sucesso")
+        is Result.Loading -> println("loading")
+        is Result.Error -> println("error")
+
+    }
+    val pessoa3 : Pessoa= Pessoa("Maria", 25, "Engenheira")
+    println(pessoa3)
+}
+open class Pessoa1 (val nome: String){
+
+}
+class Cliente (nome: String) : Pessoa1 (nome){
+
+}
+sealed class Result {
+    data class Sucess(val message: String) : Result()
+    data class Error(val error: String) : Result()
+    data object Loading : Result()
+}
+
+//
+fun main4 () {
+    println("Digite o nome da pessoa:")
+    val nome = readLine() ?: ""
+    println("Digite a idade da pessoa:")
+    val idade = readLine()?.toIntOrNull() ?: 0
+    println("Digite a profissão da pessoa:")
+    val profissao = readLine() ?: ""
+    println("Digite o CPF da pessoa:")
+    val cpf = readLine() ?: ""
+
+    val p1 = Pessoa (nome, idade, profissao, cpf)
+    val p2 = Pessoa (nome, idade, profissao, cpf)
+
+    println(Pessoa(nome, idade, profissao, cpf))
+
+}
+data class Pessoa ( val name : String,
+                    val age : Int,
+                    val profession : String,
+                    val cpf : String)
+
 
 
 
